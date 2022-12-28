b279b52ab5efded4fb951d1e6ce3841cfdd6387d
neshb <naresh.barfa@launchventures.co>
2022-12-28 18:28:09 +0530

    deep checking of object

---
+++ JS-project/test-money03.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+class Money {
+    constructor(amount, currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+    times(multiplier) {
+        return new Money(this.amount * multiplier, this.currency)
+    }
+    divide(divisor) {
+        return new Money(this.amount / divisor, this.currency)
+    }
+}
+
+let originalMoney = new Money(4002, "KRW")
+let actualMoneyAfterDivision = originalMoney.divide(4)
+let expectedMoneyAfterDivision = new Money(1000.5, "KRW")
+
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision)
