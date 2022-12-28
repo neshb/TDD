533ba75074424aab65f2c9c5f8f154be8b7e6515
neshb <naresh.barfa@launchventures.co>
2022-12-28 18:14:11 +0530

    step 2 green

--- JS-project/test-money02.js
+++ JS-project/test-money02.js
@@ -1,5 +1,14 @@
 const assert = require('assert');
 
+class Money {
+    constructor(amount, currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+    times(multiplier) {
+        return new Money(this.amount * multiplier, this.currency)
+    }
+}
 
 let tenEur = new Money(10, 'Eur');
 
